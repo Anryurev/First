4966b69fff229801e521fdd827eb99d32276debb
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-10-03 16:47:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button: Button = findViewById<Button>(R.id.button)
+    }
+}
